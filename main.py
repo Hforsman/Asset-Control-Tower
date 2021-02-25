08be2262edfd75473eed25bbe15645bde64eeb40
Hajo Forsman <hajo.forsman@outlook.com>
2021-02-25 22:11:52 +0100

    feat: read csv per row and fix short rows

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import gzip
 from io import BytesIO
 import os
@@ -9,7 +10,7 @@ import requests
 import constants
 
 
-def mkdir(directory):
+def mkdir(directory: str):
     """
     Create a requested directory in the current project if that directory does not exist yet
 
@@ -23,7 +24,7 @@ def mkdir(directory):
         print(f"Created directory {directory}")
 
 
-def download_and_save_gzip_from_url(source, destination):
+def download_and_save_gzip_from_url(source: str, destination: str):
     """
     Download a gzip file from source, decompress the archive and store it at the destination
 
@@ -47,9 +48,25 @@ def download_and_save_gzip_from_url(source, destination):
         print(f"Provided source {source} is not a gzipped archive. Source not downloaded.")
 
 
+def read_data_into_list():
+    data_list = list()
+    with open("data/vehicle.csv0001_part_00", "rb") as f:
+        reader = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        for row in reader:
+            if len(row) < 36:
+                row = fix_short_row(row=row)
+            data_list.append(row)
+
+
+def fix_short_row(row: list):
+    new_row = list()
+    for item in row:
+        new_row.extend(item.split(','))
+    return new_row
+
+
 if __name__ == '__main__':
 
     for file in constants.FILES:
         # Download, extract and save the files to disk
         download_and_save_gzip_from_url(source=file, destination="data")
-
