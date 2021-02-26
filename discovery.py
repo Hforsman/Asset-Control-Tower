1d77ee55693b6c3f38cefa33ca24f5c136f57d5d
Hajo Forsman <hajo.forsman@outlook.com>
2021-02-26 20:17:27 +0100

    add more hopefully useful stuff

--- discovery.py
+++ discovery.py
@@ -4,13 +4,21 @@ These commands are not meant to be run in any pipeline and are not build to work
 """
 
 # Import via pandas.read_csv failed because some lines were too long, others too short
+# most issues concatenate the fields type and trim because of wheel sizes in inches ("),
+# a few concatenate colour and bodytype probably because of typos
+i=0
 for row in data_list:
     if len(row) != 36:
         print(row)
         print(len(row))
+        i+=1
+print(i)
 
 # The too long line is a computer...
 for row in data_list:
     if row[4] == "NC6320":
         print(row)
         print(len(row))
+
+
+source = 'http://www.awitness.org/prophecy.zip' # for unit test on downloading a zip file
